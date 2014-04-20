f500373a3d0b550692863e17d3f822bd385c5818
Chris Allen <cehallen@gmail.com>
2014-04-20 10:59:10 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bscsails::Application.config.secret_key_base = '5f14e9d88dd694d0d21d08e90edcfb5390ec7ae43d860117e99ba40e75cf83f1c7dca237f5032a7a99f6e959e0a147dd7afb40de3ab1acd35d83508c4c4080aa'
