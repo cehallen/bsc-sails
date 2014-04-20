8322a75b9a36f0ba9bcba19c4d54a18a16496c47
Chris Allen <cehallen@gmail.com>
2014-04-20 15:12:38 -0400

    Small changes to new form

--- app/assets/javascripts/sails.js
+++ app/assets/javascripts/sails.js
@@ -1,10 +1,5 @@
 $(function() {
  return $('#sail_when').datepicker({
-   dateFormat: 'yy-mm-dd'
+   dateFormat: 'yy-mm-dd',
  });
 });
-
-// article_published_on
-// $(function() {
-//   $('.datepicker').datepicker();
-// });
