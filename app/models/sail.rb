d182eff71ba426acb6e221d4a4e81ada2e60e21c
Chris Allen <cehallen@gmail.com>
2014-04-27 15:34:20 -0400

    Added sail user spec

--- app/models/sail.rb
+++ app/models/sail.rb
@@ -2,6 +2,7 @@ class Sail < ActiveRecord::Base
   validates :when, presence: true
   validates :details, presence: true
   validates :user, presence: true
+  validates :title, presence: true
 
   belongs_to :user
 end
--- spec/models/sail_spec.rb
+++ spec/models/sail_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Sail do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { should have_valid(:when).when(Date.today) }
+    it { should_not have_valid(:when).when(nil) }
+
+    it { should have_valid(:details).when('ABC') }
+    it { should_not have_valid(:details).when(nil, '') }
+
+    it { should have_valid(:title).when('Making good progress') }
+    it { should_not have_valid(:title).when(nil, '') }
+  end
+
+  context 'associations' do
+    it { should belong_to(:user) }
+  end
 end
